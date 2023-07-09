d1b2a7506c0348701293bdc582eea41436dbfe63
Aung Ko Min <aung_ko_min@transitlink.com>
2023-07-09 20:59:30 +0800

    Initial Commit

---
+++ ImageEditor/ImageEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageEditorApp.swift
+//  ImageEditor
+//
+//  Created by Aung Ko Min on 9/7/23.
+//
+
+import SwiftUI
+
+@main
+struct ImageEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
